f4b0dd0dbc211d477c6b7c3667317247ead37280
Lindsay Warr <iamalion@gmail.com>
2023-08-18 14:51:43 -0700

    Start over

--- AnimalShelterClient/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-#nullable enable
-namespace AnimalShelterClient.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
